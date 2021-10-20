49b68b942c01e7591f268fc7581a9439755d194d
Andrej Alpatov <inf3407@hs-worms.de>
2021-10-20 20:46:59 +0200

    code: add two array of classes cStreckenAbschnitt, cStreckenEndPunkt and call the method eingabe() of the first seven elements both arrays

--- SemesterProjectWS20212022/main.cpp
+++ SemesterProjectWS20212022/main.cpp
@@ -7,7 +7,13 @@
 
 int main() {
 
+	cStreckenAbschnitt array_of_route_sections[10];
+	cStreckenEndPunkt array_of_route_end_points[10];
 
+	for (int i = 0; i < 7; i++) {
+		array_of_route_sections[i].eingabe();
+		array_of_route_end_points[i].eingabe();
+	}
 
 
 	return 0;
