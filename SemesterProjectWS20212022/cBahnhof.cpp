5ffb78f956edc43586380860b030a994fec1078f
Andrej Alpatov <inf3407@hs-worms.de>
2021-11-12 18:11:09 +0100

    code: add class cBahnhof

--- SemesterProjectWS20212022/cBahnhof.cpp
+++ SemesterProjectWS20212022/cBahnhof.cpp
@@ -1,11 +1,14 @@
-#include "cBetriebshof.h"
+#include "cBahnhof.h"
 
 /// <summary>
 /// Standard constructor
 /// </summary>
 /// <param name="bezeichnung">The name of the station</param>
 /// <param name="number_of_tracks">The number of tracks, that can be simultaneously on the station</param>
-cBetriebshof::cBetriebshof(string bezeichnung, int number_of_tracks): cStreckenEndPunkt(bezeichnung)
+/// <param name="min_stop_duration">The minimal duration of train stop</param>
+cBahnhof::cBahnhof(string bezeichnung, int number_of_tracks, int min_stop_duration) : cStreckenEndPunkt(bezeichnung)
 {
 	this->number_of_tracks = number_of_tracks;
+	this->min_stop_duration = min_stop_duration
+		;
 }
