5b47658b7bba6dc0858a15453bf9e636ca088add
AndrejAlpatov <74138987+AndrejAlpatov@users.noreply.github.com>
2021-12-16 23:53:51 +0100

    project: add other files

--- SemesterProjectWS20212022/main.cpp
+++ SemesterProjectWS20212022/main.cpp
@@ -11,6 +11,7 @@
 #include "cBetriebshof.h"
 #include "cGueterzug.h"
 #include "cPersonenzug.h"
+#include "global.h"
 
 #define ARRAY_SIZE 2
 #define NUMBER_OF_ELEMENTS_FOR_INPUT 2 //Must be <= ARRAY_SIZE
@@ -53,32 +54,13 @@ int main() {
 		<< endl;
 	cout << "Ende" << endl;
 
-	cPersonenwagen waggons_for_passenger_train[] = {  cPersonenwagen(100, 40000.0), cPersonenwagen(200, 50000.0),
-											cPersonenwagen(100, 40000.0), cPersonenwagen(100, 40000.0),
-											cPersonenwagen(100, 40000.0), cPersonenwagen(100, 40000.0),
-											cPersonenwagen(200, 50000.0), cPersonenwagen(200, 50000.0),
-											cPersonenwagen(200, 50000.0), cPersonenwagen(200, 50000.0)};
-							
-	cGueterwagen waggons_for_freight_train[] = { cGueterwagen(10000.0, 60000.0), cGueterwagen(10000.0, 60000.0),
-													cGueterwagen(10000.0, 60000.0), cGueterwagen(10000.0, 60000.0),
-													cGueterwagen(10000.0, 60000.0), cGueterwagen(10000.0, 60000.0),
-													cGueterwagen(10000.0, 60000.0), cGueterwagen(10000.0, 60000.0),
-													cGueterwagen(10000.0, 60000.0), cGueterwagen(10000.0, 60000.0),
-													cGueterwagen(20000.0, 70000.0), cGueterwagen(20000.0, 70000.0),
-													cGueterwagen(20000.0, 70000.0), cGueterwagen(20000.0, 70000.0),
-													cGueterwagen(20000.0, 70000.0), cGueterwagen(20000.0, 70000.0),
-													cGueterwagen(20000.0, 70000.0), cGueterwagen(20000.0, 70000.0),
-													cGueterwagen(20000.0, 70000.0), cGueterwagen(20000.0, 70000.0),
-													cGueterwagen(30000.0, 80000.0), cGueterwagen(30000.0, 80000.0), 
-													cGueterwagen(30000.0, 80000.0), cGueterwagen(30000.0, 80000.0),
-													cGueterwagen(30000.0, 80000.0), cGueterwagen(30000.0, 80000.0),
-													cGueterwagen(30000.0, 80000.0), cGueterwagen(30000.0, 80000.0),
-													cGueterwagen(30000.0, 80000.0), cGueterwagen(30000.0, 80000.0)};
 
 	cZug* array_of_trains[] = {
 		new cPersonenzug (waggons_for_passenger_train, 100.0, 10000.0, 60000.0, "rb31"),
 		new cGueterzug(waggons_for_freight_train, 20000.0, 20000.0, 87000.0, "rb35")
 	};
+
+
 	
 
 
